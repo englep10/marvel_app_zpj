abda40c70c328a1aad316779c16b8e90fca35a49
Josh Young <joshuaslyoung@gmail.com>
2015-03-15 14:24:50 -0400

    marvel app with working search

--- app/controllers/marvel_controller.rb
+++ app/controllers/marvel_controller.rb
@@ -3,17 +3,18 @@ class MarvelController < ApplicationController
 	def index
   		if params[:search]
     	search = params[:search].gsub(' ', '+')
-    	response = HTTParty.get("http://gateway.marvel.com/v1/public/characters?apikey=5e665d8cd10204f361468c880972d41b")
+      response = HTTParty.get("http://gateway.marvel.com/v1/public/characters?name=#{search}&ts=1&apikey=5e665d8cd10204f361468c880972d41b&hash=74d6bf483eaaa4692876c00eb0fa9e31")
     	results = JSON.parse(response.body)
-    	@results = results['name']
+    	@results = results['data']['results']
   		else
    		@results = []
   		end
 	end
 
 	def show
-  		response = HTTParty.get("http://gateway.marvel.com/v1/public/characters/#{params[:id]}")
+  		response = HTTParty.get("http://gateway.marvel.com/v1/public/comics/characters/#{params[:id]}")
   		@result = JSON.parse(response.body)
+
 	end
 
 end
